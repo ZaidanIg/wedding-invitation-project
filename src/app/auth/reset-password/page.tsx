9bbdae192441a281ae339192755053efb028f524
ZaidanIg <zaidanikhsan.g@gmail.com>
2026-05-15 22:56:59 +0700

    fix import in reset-password

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import ResetPasswordClient from './ResetPasswordClient.tsx';
+import ResetPasswordClient from './ResetPasswordClient';
 
 export default function Page() {
   return (
